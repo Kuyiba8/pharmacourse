e498fc83c284bb58d0d81a1a43b0dd555883e5ab
kuyiba8 <kuyibanthony88@gmail.com>
2022-12-05 10:45:22 -0800

    initial commit

---
+++ function.js
@@ -0,0 +1,12 @@
+/*let number = 10;
+
+function increase(number){
+    number++;
+
+}
+ increase(number);
+ console.log(number);*/
+
+
+console.log("hello kuyiba anthony");
+console.log("you are welcome");
---
+++ kuyiba.js
@@ -0,0 +1,12 @@
+function dropdownMenu(){
+    var x = document.getElementById("dropdownClick");
+    if(x.className === "topnav"){
+        x.className += " responsive";
+    }
+    else{
+        x.className = "topnav";
+
+    }
+}
+
+/*alert("hello");*/
